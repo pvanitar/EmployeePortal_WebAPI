f5ba860fe45be20601e87b7429fbe62daeea4223
pvanitar <pvanitar@gmail.com>
2025-03-24 11:23:42 +0530

    Dotnet core WebAPI to perform CRUD operation (Code First Approach)

---
+++ EmployeePortal/Controllers/EmployeeController.cs
@@ -0,0 +1,59 @@
+﻿using EmployeePortal.Models;
+using EmployeePortal.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeePortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeRepository employeeRepository;
+        public EmployeeController(EmployeeRepository employeeRepository) { 
+            this.employeeRepository = employeeRepository;   
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> EmployeeList()
+        {
+            var allEmployees=await employeeRepository.GetAllEmployees();
+            return Ok(allEmployees);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddEmployee(Employee emp)
+        {
+            await employeeRepository.SaveEmployee(emp); 
+            return Ok(emp);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateEmployee(int id, Employee emp)
+        {
+            try
+            {
+                await employeeRepository.UpdateEmployee(emp, id);
+                return Ok(new { Message = "Employee updated successfully" });
+            }
+            catch (Exception ex)
+            {
+                return NotFound(new { Message = "Employee not found", Error = ex.Message });
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteEmployee(int id)
+        {
+            try
+            {
+                await employeeRepository.DeleteEmployee(id);
+                return Ok(new { Message = "Employee deleted successfully" });
+            }
+            catch (Exception ex)
+            {
+                return NotFound(new { Message = "Employee not found", Error = ex.Message });
+            }
+        }
+    } 
+}
---
+++ EmployeePortal/Repository/EmployeeRepository.cs
@@ -0,0 +1,57 @@
+﻿using EmployeePortal.Data;
+using EmployeePortal.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeePortal.Repository
+{
+    public class EmployeeRepository
+    {
+        private readonly AppDbContext _context;
+        public EmployeeRepository(AppDbContext dbContext) {
+            this._context = dbContext;
+        }
+
+        public async Task<List<Employee>> GetAllEmployees()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        public async Task SaveEmployee(Employee emp)
+        {
+            await _context.Employees.AddAsync(emp);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateEmployee(Employee emp, int id)
+        {
+            var existingEmployee = await _context.Employees.FindAsync(id);
+
+            if (existingEmployee == null)
+            {
+                throw new Exception("Employee not found");
+            }
+
+            existingEmployee.Name = emp.Name;
+            existingEmployee.Email = emp.Email;
+            existingEmployee.Mobile = emp.Mobile;
+            existingEmployee.Age = emp.Age;
+            existingEmployee.Salary = emp.Salary;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteEmployee(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                throw new Exception("Employee not found");
+            }
+
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
